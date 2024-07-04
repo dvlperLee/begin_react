a3d8ab43c3fd4bbdc1890a49231a01fdcfc4d179
leeyangho <zktmxlit@gmail.com>
2024-07-04 09:59:23 +0900

    메뉴 level에 따른 들여쓰기 및 on off 추가

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -6,17 +6,87 @@ import { Link } from 'react-router-dom';
 
 const menu = [
 
-  {name : '홈', id: '/home', url: '/home'},
+  {
 
-  {name : '소개', id: '/about', url: '/about'},
+    name: '대메뉴1', id: 'main1', subMenu: [
 
-  {name : '서비스', id: '/services', url: '/services'},
+      { name: '중메뉴1-1', id: 'sub1-1', subMenu: [
 
-  {name : '연락처', id: '/contact', url: '/contact'},
+        { name: '소메뉴1-1-1', id: 'sub1-1-1', url: '/sub1-1-1' },
 
-  {name : '블로그', id: '/blog', url: '/blog'}
+        { name: '소메뉴1-1-2', id: 'sub1-1-2', url: '/sub1-1-2' }
+
+      ]},
+
+      { name: '중메뉴1-2', id: 'sub1-2', url: '/sub1-2' }
+
+    ]
+
+  },
+
+  {
+
+    name: '대메뉴2', id: 'main2', subMenu: [
+
+      { name: '중메뉴2-1', id: 'sub2-1', url: '/sub2-1' },
+
+      { name: '중메뉴2-2', id: 'sub2-2', url: '/sub2-2' }
+
+    ]
+
+  }
 
 ];
+
+
+
+const MenuItem = ({ item, level = 0 }) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+
+
+  const toggleSubMenu = () => {
+
+    setIsOpen(!isOpen);
+
+  };
+
+
+
+  return (
+
+    <div key={item.id} className="menu-item" style={{ paddingLeft: `${level * 20}px` }}>
+
+      {item.url ? (
+
+        <Link to={item.url}>{item.name}</Link>
+
+      ) : (
+
+        <span onClick={toggleSubMenu} style={{ cursor: 'pointer' }}>{item.name}</span>
+
+      )}
+
+      {item.subMenu && isOpen && (
+
+        <div className="sub-menu">
+
+          {item.subMenu.map(subItem => (
+
+            <MenuItem key={subItem.id} item={subItem} level={level + 1} />
+
+          ))}
+
+        </div>
+
+      )}
+
+    </div>
+
+  );
+
+};
 
 
 
@@ -28,11 +98,7 @@ const MenuList = () => {
 
       {menu.map(item => (
 
-        <div key={item.id} className="menu-item">
-
-          <Link to={item.url}>{item.name}</Link>
-
-        </div>
+        <MenuItem key={item.id} item={item} />
 
       ))}
 
